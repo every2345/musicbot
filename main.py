c1a77fd4c4c51f964399065bfa7462eb8e4117fc
every2345 <105989202+every2345@users.noreply.github.com>
2024-08-05 08:27:17 +0700

    Update main.py
    Big fix + add Playlist

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+from pickle import TRUE
 import discord
 from discord.ext import commands
 import yt_dlp
 import asyncio
+from charaiPY.AsyncPyCAI2 import PyAsyncCAI2 # IMPORT THE LIB
+import tls_client as tls # IMPORT LIB
+import re
+import random
 
 intents = discord.Intents.all() 
 bot = commands.Bot(command_prefix='!', intents=intents) #Syntax for bot using command
 music_queue = [] #Music queue to contain music list
 authorized_users = []  #To save the user permission
+pausemusic = False
+
+owner_id = 'Owner_Token' # TOKEN 
+char = "Char_ID" # CHAR ID
+chat_id = "Chat_ID" # CHAT ID
+client = PyAsyncCAI2(owner_id) # IMPORT OWNER ID
+vol = 100 #default volume
 
 @bot.event
 async def on_ready():
@@ -16,7 +28,7 @@ async def on_ready():
 async def play_music(ctx, url):
     ydl_opts = {
         'format': 'bestaudio/best',
-        'audio-format': 'mp3',
+        'audio-format': 'wav',
         'quiet': True,
         'restrictfilenames': True,
         'nocheckcertificate': True,
@@ -27,7 +39,7 @@ async def play_music(ctx, url):
         'retries': 10,
         'retry_on_error': True,
         'hls_prefer_native': True,
-        'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '320'}],
+        'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'wav', 'preferredquality': '320'}],
         'no_warnings': True
          }
 
@@ -37,17 +49,19 @@ async def play_music(ctx, url):
         source = discord.FFmpegPCMAudio(URL, before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5")
 
     voice_client = ctx.guild.voice_client
-    if voice_client is not None:
+    if voice_client is not None: #Voice client still available
         if not voice_client.is_playing():
-            voice_client.play(source)
-            await ctx.send(f'Đang chơi nhạc: {info["title"]} :minidisc:')
+            voice_client.play(discord.PCMVolumeTransformer(source))
+            player = voice_client.source
+            player.volume = vol / 100
+            await ctx.send(f'Đang chơi nhạc: {info["title"]} :minidisc:, âm lượng {vol}%')
 
-        while voice_client.is_playing():
+        while voice_client.is_playing() or voice_client.is_paused():
             await asyncio.sleep(0.5)
         
-        if len(music_queue) > 0:
+        if len(music_queue) >= 0 and not pausemusic:
             music_queue.pop(0)  # Remove the currently playing song
-            if len(music_queue) > 0:
+            if len(music_queue) >= 0:
                 await play_music(ctx, music_queue[0]['webpage_url'])  # Play the next song
             else:
                 voice_client = ctx.guild.voice_client
@@ -81,6 +95,7 @@ async def guide(ctx):
         await ctx.send('''Hướng dẫn sửa dụng bot và các lệnh cơ bản
 - Lưu ý: Bạn phải cấp quyền người dùng chỉ định cho bot để có thể sử dụng các lệnh.
 - Thông tin các lệnh hiện tại của bot:
+- !chat: Sử dụng để trò chuyện tới mình ₍^ >ヮ<^₎                       
 - !join: Sử dụng để mời bot vào kênh thoại.
 - !permission <id user>: Sử dụng để cấp quyền sử dụng bot cho một người dùng chỉ định
 - !play <link youtubbe>: Sử dụng để chơi nhạc thông qua địa chỉ video youtube được chỉ định
@@ -90,6 +105,7 @@ async def guide(ctx):
 - !volume: Tăng giảm âm ượng bài nhạc đang phát hiện tại (Giới hạn âm lượng 0 - 100)
 - !pause: Tạm dừng bài nhạc đang phát
 - !resume: Tiếp tục bài nhạc đang phát
+- !shuffle: Trộn danh sách phát nhạc                       
 - !stop: Dừng nhạc và thoát bỏ (khuyến nghị sử dụng khi không còn xài bot nữa)''')
 
 #command to permission who can use this bot
@@ -117,40 +133,112 @@ async def permission(ctx, member: commands.MemberConverter):
 
     authorized_users.append(member)
     await ctx.send(f"{member.name} đã được ủy quyền để sử dụng bot.")
+    
+@bot.command()
+async def chat(ctx, *, message: str):
+    # The message will be obtained from the command parameters rather than input
+    author_id = str(ctx.author.id)  # Get the author's ID from the context
+    message_content = message
+
+    aut_set ={
+        "author_id": "Creator_ID", # CREATOR ID
+        "is_human": True,
+        "name": "<WRITE YOUR C.AI NAME>"
+    }
+
+    async with client.connect(owner_id) as chat2:
+        r = await chat2.send_message(char, chat_id, message_content, aut_set, Return_name=True)
+        # Send message content obtained from the command
+        # Print the response
+        await ctx.send(r[16:])
 
 #command "!play" to play the music 
 @bot.command()
 async def play(ctx, url):
-    #Check Syntax
-    if not url.startswith("https://www.youtube.com/watch?v="):
-        await ctx.send("Cú pháp không hợp lệ. Vui lòng nhập một liên kết YouTube hợp lệ.")
-        return
-
-    #Check bot 
-    voice_client = ctx.guild.voice_client
-    if voice_client is None or not voice_client.is_connected():
-        await ctx.send("Bot chưa được mời vào kênh thoại...")
-        return
-
+    # Check if the URL is a valid YouTube playlist
     if ctx.author not in authorized_users:
         await ctx.send(f"{ctx.author.mention}, bạn không được phép sử dụng lệnh này.")
         return
 
+    if re.match(r'^https?://(?:www\.)?(?:youtube\.com/playlist\?list=)([\w-]+)', url):
+        await handle_playlist(ctx, url)
+    elif re.match(r'^https?://(?:www\.)?(?:youtube\.com/watch\?v=|youtu\.be/)([\w-]+)', url):
+        await handle_song(ctx, url)
+    else:
+        await ctx.send("Cú pháp không hợp lệ. Vui lòng nhập một liên kết YouTube hợp lệ. Đếu đây là dang sách, hãy kiểm tra xem dang sách đó có công khai hay không?")
+        
+#function to play list
+async def handle_playlist(ctx, url):
     ydl_opts = {
         'format': 'bestaudio/best',
-        'audio-format': 'mp3',
+        'audio-format': 'wav',
         'quiet': True,
         'restrictfilenames': True,
-         }
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        playlist_info = ydl.extract_info(url, download=False)
+        for entry in playlist_info['entries']:
+            music_queue.append(entry)
+
+    if len(music_queue) > 0:
+        await play_music(ctx, music_queue[0]['webpage_url'])
+    else:
+        await ctx.send(f'Danh sách nhạc "{playlist_info["title"]}" đã được thêm vào hàng đợi.')
+
+#function to play single song
+async def handle_song(ctx, url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'audio-format': 'wav',
+        'quiet': True,
+        'restrictfilenames': True,
+    }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=False)
         music_queue.append(info)
 
-    if len(music_queue) == 1:
+    if len(music_queue) > 0:
         await play_music(ctx, info['webpage_url'])
     else:
         await ctx.send(f'Bài nhạc "{info["title"]}" đã được thêm vào hàng đợi.')
+        
+#Command "!pause" to pause the current music playing
+@bot.command()
+async def pause(ctx):
+    global pausemusic 
+    
+    pausemusic = True
+    voice_client = ctx.guild.voice_client
+    if ctx.author not in authorized_users:
+        await ctx.send(f"{ctx.author.mention}, bạn không được phép sử dụng lệnh này.")
+        return
+
+    if voice_client.is_playing():
+        voice_client.pause()
+        await ctx.send("Bài nhạc đã được tạm dừng.")
+    else:
+        await ctx.send("Bot không đang phát bài nhạc.")
+        
+#Command "!resume" to continue the current music playing
+@bot.command()
+async def resume(ctx):
+    global pausemusic 
+    
+    pausemusic = False
+    voice_client = ctx.guild.voice_client
+    if ctx.author not in authorized_users:
+        await ctx.send(f"{ctx.author.mention}, bạn không được phép sử dụng lệnh này.")
+        return
+
+
+    if voice_client.is_paused():
+        voice_client.resume()
+        await ctx.send("Bài nhạc đã được tiếp tục.")
+        await play_music(ctx, music_queue[0]['webpage_url']) 
+    else:
+        await ctx.send("Bot đang không bị tạm dừng.")
 
 #command "!show" to show all music availabel in queue
 @bot.command()
@@ -168,6 +256,20 @@ async def show(ctx):
         for i, song in enumerate(music_queue):
             queue_msg += f"{i+1}. {song['title']} - {song['uploader']} ({song['duration']})\n"
         await ctx.send(queue_msg)
+
+#command "!shuffle" to shuffle music playlist
+@bot.command()
+async def shuffle(ctx):
+    if ctx.author not in authorized_users:
+        await ctx.send(f"{ctx.author.mention}, bạn không được phép sử dụng lệnh này.")
+        return
+
+    if len(music_queue) < 1:
+        await ctx.send("Cần ít nhất 2 bài nhạc để trộn.")
+        return
+
+    random.shuffle(music_queue)
+    await ctx.send("Đã trộn ngẫu nhiên thứ tự các bài nhạc trong hàng đợi.")
 
 #Command "!clear" to clear all music in queue
 @bot.command()
@@ -198,15 +300,16 @@ async def skip(ctx):
         return
 
     if voice_client.is_playing():
-        if len(music_queue) > 1:
+        if len(music_queue) >= 0:
             voice_client.pause()
             await ctx.send("Bot đã chuyển sang bài nhạc khác trong hàng đợi.")
             music_queue.pop(0)
-            if len(music_queue) > 0:
+            if len(music_queue) >= 0:
                 next_song_info = music_queue[0]
                 await play_music(ctx, next_song_info['webpage_url'])
         else:
             await ctx.send("Hết bài nhạc trong hàng đợi.")
+            
     elif len(music_queue) > 0:
         next_song_info = music_queue[0]
         await play_music(ctx, next_song_info['webpage_url']) 
@@ -214,54 +317,29 @@ async def skip(ctx):
         await ctx.send("Hàng đợi nhạc rỗng.")
 
 #Command "!volume" to change the music volume
-@bot.command()
-async def volume(ctx, vol: int):
-    if not 0 <= vol <= 100:
+@bot.command()        
+async def volume(ctx, volume: int):
+    global vol 
+    if not 0 <= volume <= 100:
         await ctx.send("Giá trị âm lượng không hợp lệ. Vui lòng nhập giá trị từ 0 đến 100.")
         return
-
+    
     voice_client = ctx.guild.voice_client
     if ctx.author not in authorized_users:
         await ctx.send(f"{ctx.author.mention}, bạn không được phép sử dụng lệnh này.")
         return
-
+    
     if voice_client and voice_client.is_playing():
         player = voice_client.source
         if not isinstance(player, discord.PCMVolumeTransformer):
             voice_client.source = discord.PCMVolumeTransformer(player)
             player = voice_client.source
-        player.volume = vol / 100
+        player.volume = volume / 100
+        vol = volume
         await ctx.send(f"Đã thiết lập âm lượng của bài nhạc thành {vol}%")
     else:
         await ctx.send("Không có bài nhạc đang được chơi trên voice channel")
 
-#Command "!pause" to pause the current music playing
-@bot.command()
-async def pause(ctx):
-    voice_client = ctx.guild.voice_client
-    if ctx.author not in authorized_users:
-        await ctx.send(f"{ctx.author.mention}, bạn không được phép sử dụng lệnh này.")
-        return
-
-    if voice_client.is_playing():
-        voice_client.pause()        
-        await ctx.send("Bài nhạc đã được tạm dừng.")
-    else:
-        await ctx.send("Bot không đang phát bài nhạc.")
-
-#Command "!resume" to continue the current music playing
-@bot.command()
-async def resume(ctx):
-    voice_client = ctx.guild.voice_client
-    if ctx.author not in authorized_users:
-        await ctx.send(f"{ctx.author.mention}, bạn không được phép sử dụng lệnh này.")
-        return
-
-    if voice_client.is_paused():
-        voice_client.resume()
-        await ctx.send("Bài nhạc đã được tiếp tục.")
-    else:
-        await ctx.send("Bot đang không bị tạm dừng.")
 
 #Command "!stop" to stop and bot will exit voice channel
 @bot.command()
@@ -278,5 +356,6 @@ async def stop(ctx):
   
     else:
         await ctx.send("Bạn cần dừng bài nhạc hiện đang chơi để sử dụng lệnh này!")
-#Key run of this bot
-bot.run('Bot token')
+
+#Token of this bot
+bot.run('Bot_Token')
